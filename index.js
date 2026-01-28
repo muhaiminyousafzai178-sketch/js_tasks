83a68045eb9fea694b75d7a3b13ef66b9d5d3620
muhaiminyousafzai178-sketch <muhaiminyousafzai178@gmail.com>
2026-01-28 13:04:25 +0500

    my array tasks done

---
+++ index.js
@@ -0,0 +1,58 @@
+// const student =["ali", "sara", "juanid", ,"muhaimin"];
+// student.forEach(name => console.log(name));
+
+
+
+// const number =[1,2,3,4,5,6,7,8,9,10];
+// const evennumber = number.filter(num => num%2 === 0)
+// console.log(evennumber)
+
+
+// const prices = [100, 200, 300];
+// const pricesWithGST = prices.map(price => price * 1.18);  
+// console.log(pricesWithGST); 
+
+// price => price * 1.18
+// Ye short form hai is function ka 👇
+// function(price) {
+    // return price * 1.18;
+// }
+
+
+//   const students = [
+//   { name: 'Ali', grade: 'B' },
+//   { name: 'Sara', grade: 'A' },
+//   { name: 'Junaid', grade: 'C' }
+// ];
+// const studentA = students.find(student => student.grade === 'A');
+// console.log(studentA); // { name: 'Sara', grade: 'A' }
+
+
+
+// const reverseArray = arr => arr.reverse();
+// const numbers = [1, 2, 3, 4, 5];
+// console.log(reverseArray(numbers)); // [5, 4, 3, 2, 1]
+
+// Sort array of ages in ascending order 31/36
+// let ages = [6,8,99,3,2,3,6,7,3,4,5,6,7,8,9];
+// let ascending = ages.slice().sort((a,b)=>b-a);
+// console.log(ascending);
+
+
+// const numbers = [1, 2, 3, 4, 5];
+// const [first, second] = numbers;
+// console.log(first, second); // 1 2
+
+// const students = [
+//   { name: 'Ali', passed: true },
+//   { name: 'Sara', passed: false },
+//   { name: 'Junaid', passed: true }
+// ];
+// const hasFailed = students.some(student => !student.passed);
+// console.log(hasFailed); // true
+
+
+// const numbers = [1, 2, 3];
+// const newNumbers = [...numbers, 4];
+// console.log(newNumbers); // [1, 2, 3, 4]
+
